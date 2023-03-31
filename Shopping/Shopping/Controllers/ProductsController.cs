da50a8cac9a0059f9a90ec40da8bf5412d39761f
Mich2504 <moralesmichelle197@gmail.com>
2023-03-31 08:11:52 -0600

    8:12

--- Shopping/Shopping/Controllers/ProductsController.cs
+++ Shopping/Shopping/Controllers/ProductsController.cs
@@ -183,8 +183,120 @@ namespace Shopping.Controllers
             }
             return View(product);
         }
+        //Agregar imagenes
+        public async Task<IActionResult> AddImage(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Product product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            AddProductImageViewModel model = new()
+            {
+                ProductId = product.Id,
+            };
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddImage(AddProductImageViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = Guid.Empty;
+                if (model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "products");
+                }
+                Product product = await _context.Products.FindAsync(model.ProductId);
+                ProductImage productImage = new()
+                {
+                    Product = product,
+                    ImageId = imageId,
+                };
+               
+            try
+                {
+                    _context.Add(productImage);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Details), new { Id = product.Id });
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(model);
+        }
 
+        //Eliminar producto
+        public async Task<IActionResult> DeleteImage(int? id)
+        {
+        if (id == null)
+            {
+                return NotFound();
+            }
+            ProductImage productImage = await _context.ProductImages
+            .Include(pi => pi.Product)
+            .FirstOrDefaultAsync(pi => pi.Id == id);
+            if (productImage == null)
+            {
+                return NotFound();
+            }
+            await _blobHelper.DeleteBlobAsync(productImage.ImageId, "products");
+            _context.ProductImages.Remove(productImage);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Details), new { Id = productImage.Product.Id });
+        }
+        //add Category
+        public async Task<IActionResult> AddCategory(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Product product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            AddCategoryProductViewModel model = new()
+            {
+                ProductId = product.Id,
+                Categories = await _combosHelper.GetComboCategoriesAsync(),
+            };
+            return View(model);
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddCategory(AddCategoryProductViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Product product = await _context.Products.FindAsync(model.ProductId);
+                ProductCategory productCategory = new()
+                {
+                    Category = await _context.Categories.FindAsync(model.CategoryId),
+                    Product = product,
+                };
+                try
+                {
+                    _context.Add(productCategory);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Details), new { Id = product.Id });
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+            return View(model);
+        }
 
 
 
